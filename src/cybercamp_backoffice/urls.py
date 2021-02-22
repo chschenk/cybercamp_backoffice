50602ecd51962e18b8998972f7491f63ec2648a7
Christopher Schenk <christopher@cschenk.net>
2021-02-22 21:40:26 +0100

    Add start page with basic infos

--- src/cybercamp_backoffice/urls.py
+++ src/cybercamp_backoffice/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include("cybercamp_backoffice.camp.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
