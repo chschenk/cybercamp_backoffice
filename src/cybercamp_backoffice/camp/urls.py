65ff3b94b41621c626ed51a0f831181772a4bce7
Christopher Schenk <christopher@cschenk.net>
2021-02-27 22:24:50 +0100

    Always allow any User for test auth

--- src/cybercamp_backoffice/camp/urls.py
+++ src/cybercamp_backoffice/camp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from cybercamp_backoffice.camp.views import StartView
+from cybercamp_backoffice.camp.views import StartView, CheckUserView
 
 
 app_name = 'camp'
@@ -7,4 +7,5 @@ app_name = 'camp'
 
 urlpatterns = [
     path('', StartView.as_view(), name='start'),
+    path('api/check-user/<uuid:user_id>', CheckUserView.as_view(), name='check_user'),
 ]
--- src/cybercamp_backoffice/camp/views.py
+++ src/cybercamp_backoffice/camp/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpResponse
 
 
 class StartView(TemplateView):
 	template_name = "camp/start.html"
+
+
+class CheckUserView(View):
+
+	def get(self, request, user_id):
+		return HttpResponse("User ok")
