49cb94dadd707d2efbb1d20e06d6af9959eb8d67
Christopher Schenk <christopher@cschenk.net>
2021-04-29 19:43:42 +0200

    Do not show any workshops from the past

--- src/cybercamp_backoffice/camp/views.py
+++ src/cybercamp_backoffice/camp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-
+from datetime import datetime
 from cybercamp_backoffice.camp.models import Map, User, Workshop
 
 
@@ -217,6 +217,9 @@ class WorkshopCreateView(LoginRequiredMixin, CreateView):
 class WorkshopListView(LoginRequiredMixin, ListView):
 	login_url = reverse_lazy('login')
 	model = Workshop
+	def get_queryset(self):
+		qs = super().get_queryset()
+		return qs.filter(end_time__gt=datetime.now()).order_by('start_time', 'name')
 
 
 class MyWorkshopListView(LoginRequiredMixin, ListView):
